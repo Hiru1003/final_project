e16f1548b0e411dc5f62ca54c5d800c1218c58bc
Hiru1003 <hirumiperera10@gmail.com>
2025-04-06 08:41:01 +0530

    error

--- Frontend/src/Pages/Feedback.js
+++ Frontend/src/Pages/Feedback.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import NavPage from '../Components/Nav';
 import Footer from '../Components/Footer';
-// import FeedbackImage from '../Assets/other/login.jpg';
 import FeedbackImage from '../Assets/other/login.jpg';
 import TextField from '@mui/material/TextField';
 import Snackbar from '@mui/material/Snackbar';
